3c249d6569be46c18c58c254ac6642c1fca87ed2
Lotharyx <lothar.hernandez@utgz.edu.mx>
2024-10-18 13:52:25 -0600

    Plantilla de vistas, fragmentos y layouts

---
+++ resources/views/layout/main_template.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laracrud</title>
+</head>
+<body style="background-color: rgb(225, 210, 180)">
+    
+    @include('fragments.navbar')
+
+    @yield('content') 
+</body>
+</html>
